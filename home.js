79f13c2878f74e11bca5925ffd8115acb094983c
Rahul Roy <9091royrahul@gmail.com>
2026-07-06 23:33:38 +0530

    modifying date

--- home.js
+++ home.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // ===== CONFIGURATION - CUSTOMIZE THESE! =====
   // CUSTOMIZE: Set the birthday date (format: 'Month Day, Year HH:MM:SS')
-  const birthdayDate = new Date("July 6, 2026 00:00:00").getTime();
+  const birthdayDate = new Date("July 8, 2026 00:00:00").getTime();
 
   // CUSTOMIZE: Change this greeting message
   const greetingText =
